4020c3c7cfa43f1bdf018e455942f6e07a576c38
Viktor <TulinVM@gmail.com>
2025-12-03 14:39:38 +0300

    Add-4

--- journal/settings.py
+++ journal/settings.py
@@ -131,7 +131,7 @@ SITE_ID = 1
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
     BASE_DIR / 'static'
